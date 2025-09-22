67e4cf3f1c1a917ea1fd1392b3e9b1397bf2ae4d
Evgenii Astapushkin <lebannehn.karnimirie@gmail.com>
2025-09-22 22:19:10 +0200

    [chore] Types update

--- package/index.ts
+++ package/index.ts
@@ -1,21 +1,46 @@
 import { useReducer } from 'react';
 
+/**
+ * Return type of the `useToggle` hook.
+ */
+type UseToggleReturn<T> = [
+	/**
+	 * Current value.
+	 */
+	T,
+	/**
+	 * Toggle function.
+	 */
+	() => void
+]
+
+/**
+ * Default toggle function.
+ * Uses boolean toggling.
+ * @param {boolean} value
+ */
 const defaultFunction = (value: boolean): boolean => !value;
 
 /**
+ * React hook to toggle a boolean value.
+ * Starts with `false` by default.
+ */
+export function useToggle<T = boolean>(): UseToggleReturn<T>;
+/**
+ * React hook to toggle a passed boolean value.
+ */
+export function useToggle<T = boolean>(initialValue: T): UseToggleReturn<T>;
+/**
+ * React hook to toggle a passed value with a custom function.
+ */
+export function useToggle<T>(initialValue: T, toggleFunction: (value: T) => T): UseToggleReturn<T>;
+/**
  * React hook to toggle a passed value with a custom function.
  * Uses boolean toggling by default.
- *
- * @param initialValue
- * @param toggleFunction
  */
-function useToggle(
+export function useToggle(
 	initialValue = false,
 	toggleFunction = defaultFunction
-): [boolean, () => void] {
-	const [state, toggleState] = useReducer(toggleFunction, initialValue);
-
-	return [state, toggleState];
+) {
+	return useReducer(toggleFunction, initialValue);
 }
-
-export default useToggle;
